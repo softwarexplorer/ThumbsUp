c42ad66b199df8baa912aa6ceb0f43b7b83f82a0
Matt Wenzel <mcwenzel@mtu.edu>
2019-02-23 13:21:41 -0500

    Added UI

--- Thumbs-Up/Thumbs-Up/ViewController.swift
+++ Thumbs-Up/Thumbs-Up/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var findRide: UIButton!
+    @IBOutlet weak var submitRide: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
